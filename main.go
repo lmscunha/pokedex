c0e82a6dd049d42c1dd538efa71ede461cfbb715
Leonardo Cunha <agile.ice4816@fastmail.com>
2025-01-04 10:52:59 -0300

    register exit cmd

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"slices"
 	"strings"
 )
@@ -14,6 +16,39 @@ func cleanInput(text string) []string {
 	})
 }
 
+func commandExit() {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	registry := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for true {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		parseIn := strings.Fields(strings.ToLower(input))
+		cmd := registry[parseIn[0]]
+
+		if value, exists := registry[cmd.name]; exists {
+			value.callback()
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
 }
